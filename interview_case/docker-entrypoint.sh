fd6b0f92e409077faebd88372c3e4a7c5efd0bc1
el_nikita@mail.ru <el_nikita@mail.ru>
2022-08-23 00:46:54 +1000

    dockerfile + orders model added

---
+++ interview_case/docker-entrypoint.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Apply database migrations
+echo "Apply database migrations"
+python manage.py migrate
+
+# Start server
+echo "Starting server"
+python manage.py runserver 0.0.0.0:8000
